c2ff06b084e567ffe5ae299607c2b826c8cbe8d6
rodrigo <rob.developer@protonmail.com>
2022-01-04 00:05:31 -0300

    add style to founder, visitor, entrepenaur and navbar views

--- app/assets/javascripts/founder_index.js
+++ app/assets/javascripts/founder_index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.counter').hide();
-    $('.counter').delay(5000).fadeIn();
+    $('.counter').delay(4000).fadeIn();
     $('.goal').hide();
-    $('.goal').delay(5000).fadeIn();
+    $('.goal').delay(4000).fadeIn();
 });
